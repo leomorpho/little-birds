c869f156afe2bf2e86a133dbb835ac88c1295b68
leonard audibert <leonard.audibert@gmail.com>
2020-03-07 15:22:45 -0800

    Django app and mongo work in docker-compose. Django app can be accessed on port 8000

---
+++ scraper-manager/scraperMgrUI/scraperMgrUI/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for scraperMgrUI project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'scraperMgrUI.settings')
+
+application = get_asgi_application()
