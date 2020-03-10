a0077545098a639f336c4fa3f4a21ab82469183a
leonard audibert <leonard.audibert@gmail.com>
2020-03-10 07:24:12 -0700

    Added go mod

---
+++ crawler/domain/model/scraped_objects.go
@@ -0,0 +1,18 @@
+package model
+
+import "time"
+
+type Classified struct {
+	ID          uint      `gorm:"primary_key" json:"id"`
+	Title       string    `json:"title"`
+	Price       string    `json:"price"`
+	Location    string    `json:"location"`
+	Description string    `json:"description"`
+	Url         string    `json:"url"`
+	PublishedAt time.Time `json:"published_at"`
+	CreatedAt   time.Time `json:"created_at"`
+	UpdatedAt   time.Time `json:"updated_at"`
+	DeletedAt   time.Time `json:"deleted_at"`
+}
+
+func (Classified) TableName() string { return "classifieds" }
