4728cb0bcbca18361f236d9ab1b41d2a82519218
leonard audibert <leonard.audibert@gmail.com>
2020-04-10 19:24:41 -0700

    Added ActionBar to UI

--- ai-engine/preprocessing_helper/preprocessing/object_builder.py
+++ ai-engine/preprocessing_helper/preprocessing/object_builder.py
@@ -1,12 +1,22 @@
 import uuid
 import html
+import os
+import logging
+import json
 from preprocessing.html_parsers import HtmlCleaner, CustomHtmlTarget, MetaWordsImprover
 from bs4 import BeautifulSoup as bs
 from typing import List
 from lxml import etree
 from .nlp.constants_en import META_WORDS_OF_INTEREST
 
+log = logging.getLogger()
+log.setLevel(logging.DEBUG)
+
 cleaner = HtmlCleaner()
+
+OUTPUT_FOLDER = "output_corpus"
+CORPUS_FILENAME = "corpus.jsonl" # output of program
+SOURCE_HTML = "source_html.json" # original input of program
 
 def pretty_clean(html_str: str) -> str:
     clean_html = cleaner.pretty_clean(html_str)
@@ -54,7 +64,7 @@ def call_pipeline(html_str: str) -> str:
         "full_text": full_text,
         "meta_words_of_interest": meta_words_of_interest,
         "meta": meta,
-        "annotation_approver": None, 
+        "annotation_approver": [], 
         "labels": [],
         "html": raw_html
     }
@@ -63,5 +73,31 @@ def call_pipeline(html_str: str) -> str:
 def pipeline_on_saved_data() -> None:
     # Add button in UI to save html (with url) to DB. Should there also be an option 
     # to add words to the metawords (like handpicked categories). This option would 
-    # need autocomplete to be sure not to create too many
-    pass
+    # need autocomplete to be sure not to create too many metawords
+    
+    return None
+
+def pipeline_and_save(html_str: str) -> None:
+    # Add button in UI to save html (with url) to DB. Should there also be an option 
+    # to add words to the metawords (like handpicked categories). This option would 
+    # need autocomplete to be sure not to create too many metawords
+    result = call_pipeline(html_str)
+    result_json = json.dumps(result, indent=4)
+    
+    if os.path.isdir(OUTPUT_FOLDER) is False:
+        os.mkdir(OUTPUT_FOLDER)
+    input_filepath = OUTPUT_FOLDER + "/" + SOURCE_HTML
+    corpus_filepath = OUTPUT_FOLDER + "/" + CORPUS_FILENAME
+    if os.path.exists(corpus_filepath) is False:
+        log.error("creating csv file")
+        open(corpus_filepath, "w").close()
+    if os.path.exists(input_filepath) is False:
+        log.error("creating csv file")
+        open(input_filepath, "w").close()
+    
+    with open(input_filepath, "a+") as f:
+        f.write(html.escape(html_str) + "\n")
+    with open(corpus_filepath, "a+") as f:
+        f.write(result_json)
+    
+    return result_json
--- ai-engine/preprocessing_helper/ui.py
+++ ai-engine/preprocessing_helper/ui.py
@@ -10,11 +10,12 @@ from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.textinput import TextInput
 from kivy.uix.button import Button
 from kivy.uix.widget import Widget
+from kivy.uix.actionbar import ActionBar
 from kivy.properties import ObjectProperty
 from kivy.core.window import Window
 from preprocessing.html_parsers import HtmlCleaner
 from preprocessing.object_builder import pretty_clean, pprint_unescape, \
-    call_pipeline, pipeline_on_saved_data
+    call_pipeline, pipeline_on_saved_data, pipeline_and_save
 
 
 # Take raw scraped html and clean it, giving it back through clipboard.
@@ -35,6 +36,7 @@ class ProcessHtml(BoxLayout):
     
     def pretty_clean(self):
         if self.html_input.text == "":
+            self.html_output.text = "You must enter html data to pretty print it"
             return
         self.html_output.text = pretty_clean(self.html_input.text)
         # pyperclip.copy(self.html_output.text)
@@ -42,6 +44,7 @@ class ProcessHtml(BoxLayout):
     
     def escape(self):
         if self.html_input.text == "":
+            self.html_output.text = "You must enter html data to escape it"
             return
         self.html_output.text = html.escape(" ".join(self.html_input.text.split()))
         pyperclip.copy(self.html_output.text)
@@ -49,6 +52,7 @@ class ProcessHtml(BoxLayout):
         
     def pprint_unescape(self):
         if self.html_input.text == "":
+            self.html_output.text = "You must enter escaped data to unescape it"
             return
         self.html_output.text = pprint_unescape(self.html_input.text)
         # pyperclip.copy(self.html_output.text)
@@ -57,6 +61,7 @@ class ProcessHtml(BoxLayout):
     # Format for training corpus
     def pipeline(self):
         if self.html_input.text == "":
+            self.html_output.text = "You must enter html data to parse it"
             return
         result = call_pipeline(self.html_input.text)
         self.html_output.text = json.dumps(result, indent=4)
@@ -65,8 +70,25 @@ class ProcessHtml(BoxLayout):
         
     # Format for training corpus
     def pipeline_on_saved_data(self):
-        result = pipeline_on_saved_data()
-        self.html_output.text = "DONE"
+        try:
+            pipeline_on_saved_data()
+        except Exception as err:
+            self.html_output.text = str(err)
+        self.html_output.text = "Done! Pipeline ran on all saved items"
+        # pyperclip.copy(self.html_output.text)
+        # pyperclip.paste()
+    
+    # Format for training corpus
+    def pipeline_and_save(self):
+        if self.html_input.text == "":
+            self.html_output.text = "You must enter html data to parse it and save it to file"
+            return
+        try:
+            result = "Done! The following item was saved to file:\n\n"
+            result += pipeline_and_save(self.html_input.text)
+            self.html_output.text = result
+        except Exception as err:
+            self.html_output.text = str(err)
         # pyperclip.copy(self.html_output.text)
         # pyperclip.paste()
         
